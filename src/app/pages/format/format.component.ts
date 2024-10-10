6de07cdf67e9796e8534b5404064785b357db6b4
dung.ngu <dung.ngu@bnksolution.com>
2024-10-10 13:40:26 +0700

    Add custom pipe

--- src/app/pages/format/format.component.ts
+++ src/app/pages/format/format.component.ts
@@ -1,12 +1,13 @@
 import { CurrencyPipe, DatePipe, DecimalPipe, PercentPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { AddStarPipe } from '../../untils/pipe_custom/add_start_pipe';
 
 @Component({
   selector: 'app-format',
   standalone: true,
   templateUrl: './format.component.html',
   styleUrl: './format.component.scss',
-  imports: [DecimalPipe, DatePipe, CurrencyPipe,PercentPipe],
+  imports: [DecimalPipe, DatePipe, CurrencyPipe,PercentPipe,AddStarPipe],
 })
 export class FormatComponent {
   num = 103.1234;
